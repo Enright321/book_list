337481f8f5c31a4a6bb85628f5c8a4a551b0a101
Enright321 <enrightjoe4@gmail.com>
2021-03-30 19:42:00 -0500

    initial

--- app.js
+++ app.js
@@ -14,39 +14,37 @@ UI.prototype.addBookToList = function (book) {
   const row = document.createElement('tr');
   // insert cols
   row.innerHTML = `
-    <td>${book.title}</td>
-    <td>${book.author}</td>
-    <td>${book.isbn}</td>
-    <td><a href="#" class="delete">X</a></td>
-    `;
+  <td>${book.title}</td>
+  <td>${book.author}</td>
+  <td>${book.isbn}</td>
+  <td><a href='#' class='delete'>X</a></td>
+  `
   list.appendChild(row);
 }
 
 UI.prototype.clearFields = function () {
-  document.getElementById('title').value = '';
-  document.getElementById('author').value = '';
-  document.getElementById('isbn').value = '';
+  document.getElementById('title').value === '';
+  document.getElementById('author').value === '';
+  document.getElementById('isbn').value === '';
 }
 
 // Event Listeners
 document.getElementById('book-form').addEventListener('submit', function (e) {
-  // get form values
   const title = document.getElementById('title').value,
     author = document.getElementById('author').value,
-    isbn = document.getElementById('isbn').value;
+    isbn = document.getElementById('isbn').value
 
   // instantiate book
   const book = new Book(title, author, isbn);
 
-  // instantiate UI
+  // instantiate ui
   const ui = new UI();
 
   // Add book to list
   ui.addBookToList(book);
 
-  // Clear Fields
-  ui.clearFields()
-
+  // clear fields
+  ui.clearFields();
 
   e.preventDefault();
-})
+});
